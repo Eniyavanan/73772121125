bde3c3662518ab08b55f3ee69f90177cf6a6394e
Eniyavanan <159708966+Eniyavanan@users.noreply.github.com>
2024-07-13 16:10:44 +0530

    Create Loop100to0.java

---
+++ Loop100to0.java
@@ -0,0 +1,8 @@
+public class Loop100to0{ 
+    public static void main(String[] args) {
+            for(int i=100;i>=0;i--){
+                System.out.print(i+" ");
+            }
+        }
+    }
+    
