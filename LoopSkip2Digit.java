02ca3ea1654db07060b5a6cb0d2837c8601ce48b
Eniyavanan <159708966+Eniyavanan@users.noreply.github.com>
2024-07-13 16:11:47 +0530

    Create LoopSkip2Digit.java

---
+++ LoopSkip2Digit.java
@@ -0,0 +1,8 @@
+public class LoopSkip2Digit{ 
+    public static void main(String[] args) {
+            for(int i=100;i>=0;i=i-3){
+                System.out.print(i+" ");
+            }
+        }
+    }
+    
